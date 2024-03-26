ff460f77dc34d9500b83ca354d0bbf47cd66c0e4
MarkGarOliver <marcosgarcia.oliveira02@gmail.com>
2024-03-26 14:02:07 -0300

    new conection settings api

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 const Environment = {
-  SUPABASE_URL: "https://eikbnmphzjoeopujpnnt.supabase.co",
+  SUPABASE_URL: "https://cxtsbhcmhrdiarquvvcr.supabase.co",
   SUPABASE_API:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpa2JubXBoempvZW9wdWpwbm50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTcyOTc5NjcsImV4cCI6MTk3Mjg3Mzk2N30.3uHt44B6-yMoePwJ2fJryZDM2vMfYfciVyNaeg_SuWA",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dHNiaGNtaHJkaWFycXV2dmNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExMjc3NTEsImV4cCI6MjAyNjcwMzc1MX0.5jrS-GsJL2dkPbdz7okExPwfBHeqbmMxLeCLDhLM8e4",
 };
 
 export default Environment;
